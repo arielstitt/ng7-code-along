2a6f90f7642885a352708ba115b5a93dd26d395d
arielstitt <arielstitt@gmail.com>
2019-01-15 14:47:20 -0500

    added get users to data services

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,14 +1,23 @@
 import { Injectable } from '@angular/core';
+//to gain access throughout the app, http client needs to be imported here
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 // serviced allow you to define code that needs to be accesable across mltiple components in a central location 
-  constructor() { }
 
-  firstClick () {
-    // we want to be able to access this from the home ts file
-    return console.log("clicked from data.services.ts")
+//create an instance of the http client in the constructor
+  constructor(private http:HttpClient) { }
+
+  // firstClick () {
+  //   // we want to be able to access this from the home ts file
+  //   return console.log("clicked from data.services.ts")
+  // }
+
+  
+  getUsers() {
+    return this.http.get('http://regres.in/api/users');
   }
 }
