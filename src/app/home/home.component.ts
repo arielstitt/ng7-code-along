807f2f647e97841788e335390c8062d3ce332d0c
arielstitt <arielstitt@gmail.com>
2019-01-15 14:50:42 -0500

    grabbing the iser data and storinn it into an object. console loggin results

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,22 +8,30 @@ import { DataService } from '../data.service';
 })
 export class HomeComponent implements OnInit {
 // here is the logic for the boolean in the html file
-  h1Style: Boolean = false;
+  //h1Style: Boolean = false;
+
+  //define the users object that is being grabbed from the api
+  users: Object;
 
   //in order to access the methods from data.service.ts
   //create an instance of it through dependancy injection in the constructor
   constructor(private data: DataService) { }
 
   ngOnInit() {
+    this.data.getUsers().subscribe(data => {
+      this.users = data;
+      console.log(this.data)
+    })
+
   }
   // This is where the logic for the click button lives
-  firstClick() {
-    console.log("clicked!")
-    // selected h1Style and makes it true
-    // this.h1Style = true;
+//   firstClick() {
+//     console.log("clicked!")
+//     // selected h1Style and makes it true
+//     // this.h1Style = true;
 
-//this.data will give you access to the data service method
-    this.data.firstClick();
+// //this.data will give you access to the data service method
+//     this.data.firstClick();
 
   }
 
