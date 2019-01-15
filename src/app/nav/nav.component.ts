ab89e7dc8e6674b2b520cc6522d641b9ef8abbcc
arielstitt <arielstitt@gmail.com>
2019-01-15 13:48:14 -0500

    first use of string interpolation in nav compoenents

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
+// below is an example of TypeScript
+// we are binding the app title property to "Ng-code along"
+// in the nav.component.html file there is an appTitle that will use interpolation
+  appTitle: String = "Ng-CodeAlong";
 
   constructor() { }
 
